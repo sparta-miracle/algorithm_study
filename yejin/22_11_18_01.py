fc66856782fc472eafaec276b56b3263dd237732
jinin1 <117060164+jinin1@users.noreply.github.com>
2022-11-18 15:31:18 +0900

    Create 22_11_18_01.py

---
+++ yejin/22_11_18_01.py
@@ -0,0 +1,13 @@
+def solution(my_string):
+    # str(my_string)
+    # for i in my_string:
+    # if i == 'a':
+    #     my_string.remove('a','e','i','o','u')
+    # answer = my_string
+    # return answer
+    
+    # answer = [item for item in my_string if item != 'a' and item != 'e' and item !='i' and item != 'o' and item != 'u']
+    # return answer
+    # print(answer)
+    
+# 처참한 실패. 다른 함수를 찾아야겠다. 
