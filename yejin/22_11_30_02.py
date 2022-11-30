03a8da6ae7e135c550b55e819c6bcfc622f514b9
jinin1 <117060164+jinin1@users.noreply.github.com>
2022-11-30 15:52:35 +0900

    Create 22_11_30_02.py

---
+++ yejin/22_11_30_02.py
@@ -0,0 +1,20 @@
+# 팩토리얼
+# 반복문을 돌려 i를 계속 1씩 커지게 만들어 a에 곱한다
+# if문을 사용해 a가 n보다 커지게 될 경우, 반복을 멈추고 i-1 값을 리턴하게 한다.
+def solution(n):
+    a = 1
+    i = 1
+    if sum > n:
+        for a in 
+            a *= i
+            i+=1
+    answer = i-1
+    return answer
+#오류: Traceback (most recent call last):
+#   File "/solution_test.py", line 6, in <module>
+#     from solution import *
+#   File "/solution.py", line 9
+#     for a in 
+#             ^
+# SyntaxError: invalid syntax
+# 내 실력이 부족해서 구현을 못하겠다. 반복문 다시 공부해야겠다.
