0623ff4ad5a18d1817b8f9d91eff758f1d8aa3ab
jinin1 <117060164+jinin1@users.noreply.github.com>
2022-12-01 15:57:32 +0900

    Update 22_12_01_02.py

--- yejin/22_12_01_02.py
+++ yejin/22_12_01_02.py
@@ -2,6 +2,7 @@
 def solution(keyinput, board):
     r = 0
     u = 0
+# 가로측 좌표 r과 세로측 좌표 u를 정한다.
     for i in keyinput:
         if i == "left":
             r -= 1
@@ -11,7 +12,7 @@ def solution(keyinput, board):
             u += 1
         else:
             u -= 1
-            
+# board 값을 통해 좌표의 증가 범위를 제한해준다.
         if r > board[0]//2:
             r = board[0]//2
         elif r < board[0]//2*-1:
