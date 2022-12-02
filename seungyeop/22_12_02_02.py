ba63ec52cf7468a311d18a8a25db0b9640d6559d
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-02 15:41:33 +0900

    Update 22_12_02_02.py

--- seungyeop/22_12_02_02.py
+++ seungyeop/22_12_02_02.py
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] solution(int[] num_list, int n) {
-        int row = (num_list.length + 1) / n;
+        int row = num_list.length / n;
         int[][] answer = new int[row][n];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < n; j++) {
