e1aa89faf06859e632f87d00516507c5a6d715b9
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-02 10:48:30 +0900

    Create 22_12_02_02.py

---
+++ seungyeop/22_12_02_02.py
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int row = (num_list.length + 1) / n;
+        int[][] answer = new int[row][n];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < n; j++) {
+                answer[i][j] = num_list[i*n+j];
+            }
+        }
+        return answer;
+    }
+}
