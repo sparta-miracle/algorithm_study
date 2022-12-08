6ae69b844da267d19fdf389ffd4251016cceef76
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-08 15:32:56 +0900

    Update 22_12_08_01.java

--- seungyeop/22_12_08_01.java
+++ seungyeop/22_12_08_01.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] solution(int[] array) {
         int[] answer = new int[2];
-        answer[0] = 0;
+        // answer[0] = 0;   // 초기값 영으로 들어옴
         for (int i = 0; i < array.length; i++) {    //하나의 for문 안에서 해결 가능 했음
             if (array[i] > answer[0]) {
                 answer[0] = array[i];
+                answer[1] = i;
             }
         }
-        for (int j = 0; j < array.length; j++) {
-            if (array[j] == answer[0]) {
-                answer[1] = j;
-            }
-        }
+        // for (int j = 0; j < array.length; j++) {
+        //     if (array[j] == answer[0]) {
+        //         answer[1] = j;
+        //     }
+        // }
         return answer;
     }
 }
