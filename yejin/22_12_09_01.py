b668dd1b9ff244fd98a271bb203b67631278cb36
jinin1 <117060164+jinin1@users.noreply.github.com>
2022-12-12 11:58:59 +0900

    Create 22_12_09_01.py

---
+++ yejin/22_12_09_01.py
@@ -0,0 +1,23 @@
+# 치킨 쿠폰
+
+def solution(chicken):
+    rest = 0 #남는 보너스 쿠폰
+    a = 0 #서비스치킨
+    while(chicken>9):        #치킨 1081 >108
+        # rest+= chicken%10    #남는쿠폰 1 >8
+        a+= chicken//10       #서비스 108 >10
+        chicken= chicken//10   #서비스치킨 108
+        chicken+= rest
+        
+    return a
+
+# def solution(chicken):
+#     rest = 0 #남는 보너스 쿠폰
+#     a = 0 #서비스치킨
+#     while(chicken>9):        #치킨 1081 >108
+#         rest+= chicken%10    #남는쿠폰 1 >8
+#         a+= round(chicken//10)      #서비스 108 >10
+#         chicken= round(chicken//10)   #서비스치킨 108
+#         chicken+= rest
+        
+#     return a
