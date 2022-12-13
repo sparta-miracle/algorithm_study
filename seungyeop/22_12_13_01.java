0db6c50d08e1422c3c5aee285677cc99fbe5c6cb
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-13 15:44:15 +0900

    Create 22_12_13_01.java

---
+++ seungyeop/22_12_13_01.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 0; i <= n; i += 2) {
+            answer += i;
+        }
+        return answer;
+    }
+}
