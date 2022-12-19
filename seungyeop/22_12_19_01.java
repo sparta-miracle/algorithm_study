0643ff99dc49340df913c05d2bacffa98cf7b2ad
milnil0228 <117057843+milnil0228@users.noreply.github.com>
2022-12-19 12:19:29 +0900

    Update 22_12_19_01.java

--- seungyeop/22_12_19_01.java
+++ seungyeop/22_12_19_01.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {    //답안을 보니 다들 유클리드 호제법을 쓰더랍니다,,
     public int[] solution(int denum1, int num1, int denum2, int num2) {
         int denum = denum1 * num2 + denum2 * num1; //분자
         int num = num1 * num2; //분모
@@ -13,3 +13,9 @@ class Solution {
         return answer;
     }
 }
+//유클리드 호제법
+// public int GCD(int num1, int num2) {
+//         if (num1 % num2 == 0)
+//             return num2;
+//         return GCD(num2, num1 % num2);
+//     }
