8464c28c3d527592c4b6889d25e16f73bb6a3fb2
jinin1 <117060164+jinin1@users.noreply.github.com>
2022-12-26 16:02:33 +0900

    Update 22_12_23_01.py

--- yejin/22_12_23_01.py
+++ yejin/22_12_23_01.py
@@ -10,3 +10,10 @@ def solution(x, n):
  # l은 answer의 마지막 수
 # 반복문과 range를 이용해, x부터 l까지 x씩 증가하는 숫자를 배열 answer에 넣는다.
 # 문제: 테스트8 런타임 에러 / 정확성 92.9
+# 해결: 0을 곱하거나 누눌 수 없기 때문에 문제가 발생하였다. 1부터 시작해보자.
+
+def solution(x, n):
+    answer = []
+    for i in range(1,n+1):
+            answer.append(i*x)
+    return answer
