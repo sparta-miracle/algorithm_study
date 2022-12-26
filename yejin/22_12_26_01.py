71ce8142f04a7e600019afbec149ed747363b213
jinin1 <117060164+jinin1@users.noreply.github.com>
2022-12-26 11:27:12 +0900

    Create 22_12_26_01.py

---
+++ yejin/22_12_26_01.py
@@ -0,0 +1,15 @@
+# 하샤드 수
+
+# 방법1
+# a=자릿수의 합 구하기(map이용)
+# x%a==0 이면, True 반환
+
+def solution(x):
+    answer = False
+    i,a = x,0
+    i = list(map(int, str(len(i))))
+    for b in range(i):
+        a += i[b]
+    if x%a ==0:
+        answer = True
+    return answer
