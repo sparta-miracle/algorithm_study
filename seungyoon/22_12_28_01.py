6fee674c876d66cfce901f85e40da3e50be9a5aa
Seungyoon Lee <lssy32@naver.com>
2022-12-28 16:10:20 +0900

    Create 22_12_28_01.py

---
+++ seungyoon/22_12_28_01.py
@@ -0,0 +1,6 @@
+def solution(arr1, arr2):
+    #answer = [[] * len(arr1[0] * len(arr1)
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            arr1[i][j] = arr1[i][j] + arr2[i][j]
+    return arr1
