134410b591f8176dd6289090b3ab9cd4682d93ce
seinHan <104081143+seinni2@users.noreply.github.com>
2023-01-11 15:28:27 +0900

    Create 23_01_11_02.java

---
+++ sein/23_01_11_02.java
@@ -0,0 +1,34 @@
+/*
+
+대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+
+예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+
+제한사항
+문자열 s의 길이 : 50 이하의 자연수
+문자열 s는 알파벳으로만 이루어져 있습니다.
+*/
+
+
+class Solution {
+    boolean solution(String s) {
+        int p_num = 0;
+        int y_num = 0;
+        
+        s = s.toUpperCase();
+        
+        if(s.contains("P")){
+          p_num = s.length() - s.replace("P", "").length();
+        }
+        
+        if(s.contains("Y")){
+          y_num = s.length() - s.replace("Y", "").length();
+        }
+      
+        if(p_num==y_num){
+            return true;
+        }
+        
+        return false;
+    }
+}
