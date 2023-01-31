cdd38744b42515002ef96048324979a05e0ef7f6
minseon33 <109520441+minseon33@users.noreply.github.com>
2023-01-31 09:34:45 +0900

    Create 23_01_26_01.py

---
+++ minseon/23_01_26_01.py
@@ -0,0 +1,11 @@
+#두 개 뽑아서 더하기
+
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)) :
+        for j in range(i+1,len(numbers)):
+            if numbers[i]+numbers[j] not in answer:
+                answer.append(numbers[i]+numbers[j])
+
+    answer.sort()
+    return answer
